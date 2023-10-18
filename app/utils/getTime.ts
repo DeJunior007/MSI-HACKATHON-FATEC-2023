6ab33eaa67fc7962c4d8ada0d33217c2954f34fc
DeJunior007 <dejunior05@hotmail.com>
2023-10-18 19:27:35 -0300

    Fix: Refatoração; simplificação para facilidade de uso

--- app/utils/getTime.ts
+++ app/utils/getTime.ts
@@ -1,30 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function getTime() {
   try {
-    const resposta = await axios.get('http://worldtimeapi.org/api/timezone/America/Sao_Paulo');
+    const resposta = await axios.get(
+      "http://worldtimeapi.org/api/timezone/America/Sao_Paulo"
+    );
 
     switch (resposta.status) {
       case 200:
-        const dataHoraBrasilia = resposta.data.datetime;
-        console.log('Hora em Brasília:', dataHoraBrasilia);
+        const dataHoraBrasilia = new Date(resposta.data.datetime);
         return dataHoraBrasilia;
       default:
-        console.error('Erro não tratado. Código de Status:', resposta.status);
+        console.error(
+          "Erro, não foi possível realizar a consulta",
+          resposta.status
+        );
         return null;
     }
   } catch (erro: any) {
-    switch (erro.response?.status) {
-      case 404:
-        console.error('Recurso não encontrado. Verifique a URL.');
-        break;
-      case 500:
-        console.error('Erro interno do servidor.');
-        break;
-      default:
-        console.error('Erro ao obter a hora em Brasília:', erro.message);
-        break;
-    }
+    console.error("Erro ao obter a hora em Brasília:", erro.message);
     return null;
   }
 }
