b59854af457e49787114279ebf848987d257a8ee
DeJunior007 <dejunior05@hotmail.com>
2023-10-17 23:46:03 -0300

    Fix: Refatoração do layout para head da pagina de login

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function LoginHead() {
+  return (
+    <>
+      <title>MSI - Login</title>
+      <meta name="description" content="Tela de login" />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,16 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'MSI Chamada',
-  description: 'Tela de login',
-}
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
