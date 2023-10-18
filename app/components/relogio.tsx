8dd3fca015f439a30b4ca5f138b1a98b9236eedb
DeJunior007 <dejunior05@hotmail.com>
2023-10-18 19:28:50 -0300

    Fix: Adição de dia da semana ao relogio

--- app/components/relogio.tsx
+++ app/components/relogio.tsx
@@ -1,37 +1,48 @@
 import { useState, useEffect } from "react";
+import { getTime } from "../utils/getTime";
 
 function zeroAEsquerda(valor: number): string {
-  return valor < 10 ? `0${valor}` : valor.toString();
+  return String(valor).padStart(2, "0");
 }
 
 export function Relogio() {
   const [minutos, setMinutos] = useState(0);
   const [horas, setHoras] = useState(0);
+  const [diaSemana, setDiaSemana] = useState("");
 
   useEffect(() => {
+    async function fetchTime() {
+      const dataHoraBrasilia = await getTime();
+      if (dataHoraBrasilia) {
+        const minutosAtuais = dataHoraBrasilia.getMinutes();
+        const horasAtuais = dataHoraBrasilia.getHours();
+        const diaSemanaAtual = dataHoraBrasilia.toLocaleString("pt-BR", {
+          weekday: "long",
+        });
+
+        setMinutos(minutosAtuais);
+        setHoras(horasAtuais);
+        setDiaSemana(diaSemanaAtual);
+      }
+    }
+
+    fetchTime();
+
     const interval = setInterval(() => {
-      atualizarTempo();
-    }, 1000);
+      fetchTime();
+    }, 60000);  
 
     return () => {
       clearInterval(interval);
     };
   }, []);
 
-  const atualizarTempo = () => {
-    const agora = new Date();
-    const minutosAtuais = agora.getMinutes();
-    const horasAtuais = agora.getHours();
-
-    setMinutos(minutosAtuais);
-    setHoras(horasAtuais);
-  };
-
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center lt:hidden sm:flex">
       <p className="text-lg">
         {zeroAEsquerda(horas)}:{zeroAEsquerda(minutos)}
       </p>
+      <p className="text-md">{diaSemana}</p>
     </div>
   );
 }
